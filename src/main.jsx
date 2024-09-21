111e9e89776242d8c21e0b3a1e4b5ea612c8fdfb
Andresyt12 <andresfelipeosorioperez894@gmail.comm>
2024-09-21 15:10:37 -0500

    Añadiendo navegacion

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,28 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import Login from './components/login'
-import FacturaPro from './components/FacturaPro'; 
-import './components/FacturaPro.css'; 
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from 'react-router-dom';
+
+import './index.css';
+import Login from './components/login';
+import FacturaPro from './components/FacturaPro';
+import './components/FacturaPro.css';
 import Registro from './components/Registro';
 
-createRoot(document.getElementById('root')).render(
+const rootElement = document.getElementById('root');
+const root = createRoot(rootElement);
+
+root.render(
   <StrictMode>
-    <Login />
-    <FacturaPro/>
-    <Registro/>
-  </StrictMode>,
-)
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/facturapro" element={<FacturaPro />} />
+        <Route path="/registro" element={<Registro />} />
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>
+);
